7f437c1c2b3063a637283227abbf146c63292b1c
Ole Schleeßelmann <likro@outlook.de>
2019-12-03 17:10:37 +0100

    adde getFibonacci

--- src/Uebung1.erl
+++ src/Uebung1.erl
@@ -10,11 +10,12 @@
 -author("likro").
 
 %% API
--export([createList/0, getVielfache/1, getLast/1, shiftRight/1]).
+-export([createList/0, getVielfache/1, getLast/1, shiftRight/1, getFibonacci/1]).
 
 %% Aufgabe 1 a)
 createList() ->
   lists:seq(0,100).
+
 
 %% Aufgabe 1 b)
 getVielfache(A) ->
@@ -39,7 +40,15 @@ addAllButLast(ListToAdd, [H|T]) ->
 %% Aufgabe 1 d)
 getLast([H|T]) ->
   getLast(T, H).
-getLast([H|T], FirstElementOfLastIteration) ->
+getLast([H|T], _) ->
   getLast(T, H);
 getLast([], FirstElementOfLastIteration) ->
   FirstElementOfLastIteration.
+
+%% Aufgabe 2 a)
+getFibonacci(N) ->
+  getFibonacci(N , 1, [0,1], 1, 0).
+getFibonacci(N, I, List, _, _) when I >= N ->
+  List;
+getFibonacci(N, I, List, LastElement, SecondLastElement) ->
+  getFibonacci(N, I + 1, List ++ [SecondLastElement + LastElement], SecondLastElement + LastElement, LastElement).
